6407a77f3cf05550dc6866464f09cb5b3c7f5324
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2018-11-21 00:17:33 -0200

    configurações

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import configuration from '../configurations/configurations'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: configuration.serverUrl
 })
 
 export default api
